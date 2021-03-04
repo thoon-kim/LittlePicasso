f2955cc5a424909d534293a8bb1b00030e82709a
honginhwa <https://github.com/honginhwa/pytorch_study>
2021-03-05 00:33:56 +0900

    google_Login_email_name

--- app/src/main/java/com/fund/littlepicasso/ui/login/GoogleLoginActivity.kt
+++ app/src/main/java/com/fund/littlepicasso/ui/login/GoogleLoginActivity.kt
@@ -1,14 +1,95 @@
-package com.fund.littlepicasso.ui.login
+package com.google.firebase.quickstart.auth.googlelogin_3
 
+
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.fund.littlepicasso.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.quickstart.auth.googlelogin_3.databinding.ActivityMainBinding
 
-class GoogleLoginActivity: AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val RC_SIGN_IN = 100
+        const val TAG = "firebaseTag"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_google_login)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInButton.setOnClickListener { signIn() }
+
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+    }
+
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        // 정보가 있으면 signinaccout 의 객체  없으면 null값
+
+        // 정보 표시 !!
+        if (account == null){
+
+            val signInIntent: Intent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        }
+        else{
+            Log.d(TAG,"account : "+ account.familyName)
+            Log.d(TAG,"account : "+ account.displayName)
+            Log.d(TAG,"account : "+ account.email)
+        }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+
+        }
     }
 
 }
