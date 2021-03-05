b48b52ed42b6fd6400e0b87b842e7c600561af7a
Taehoon <50002966+thoon-kim@users.noreply.github.com>
2021-03-05 13:38:09 +0900

    인스타그램 로그인 추가

--- app/src/main/java/com/fund/littlepicasso/ui/login/InstaLoginActivity.kt
+++ app/src/main/java/com/fund/littlepicasso/ui/login/InstaLoginActivity.kt
@@ -2,6 +2,11 @@ package com.fund.littlepicasso.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.AccessToken
 import com.facebook.CallbackManager
@@ -16,51 +21,72 @@ import kotlinx.android.synthetic.main.activity_insta_login.*
 class InstaLoginActivity: AppCompatActivity() {
 
     lateinit var callbackManager: CallbackManager
+    var redirectUrl = "https://socialsizzle.herokuapp.com/auth/" // TODO redirect url 수정
+    var url = "https://api.instagram.com/oauth/authorize" +
+            "?client_id=857265938463278" +
+            "&redirect_uri=$redirectUrl" +
+            "&scope=user_profile" +
+            "&response_type=code"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insta_login)
 
-        login_button.setReadPermissions("email")
+        var mWebClient = WebClient()
+        web_view.loadUrl(url)
+        web_view.webViewClient = mWebClient
+        web_view.settings.javaScriptEnabled = true
 
-        callbackManager = CallbackManager.Factory.create()
-
-        // Callback registration
-        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
-            override fun onSuccess(loginResult: LoginResult?) {
-                // App code
-            }
-
-            override fun onCancel() {
-                // App code
-            }
-
-            override fun onError(exception: FacebookException) {
-                // App code
-            }
-        })
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-            object : FacebookCallback<LoginResult?> {
-                override fun onSuccess(loginResult: LoginResult?) {
-                    // App code
-                    var accessToken: AccessToken = AccessToken.getCurrentAccessToken()
-                    var isLoggedIn: Boolean = accessToken != null && !accessToken.isExpired
-                }
-
-                override fun onCancel() {
-                    // App code
-                }
-
-                override fun onError(exception: FacebookException) {
-                    // App code
-                }
-            })
+//        login_button.setReadPermissions("email")
+//
+//        callbackManager = CallbackManager.Factory.create()
+//
+//        // Callback registration
+//        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+//            override fun onSuccess(loginResult: LoginResult?) {
+//                // App code
+//            }
+//
+//            override fun onCancel() {
+//                // App code
+//            }
+//
+//            override fun onError(exception: FacebookException) {
+//                // App code
+//            }
+//        })
+//
+//        LoginManager.getInstance().registerCallback(callbackManager,
+//            object : FacebookCallback<LoginResult?> {
+//                override fun onSuccess(loginResult: LoginResult?) {
+//                    // App code
+//                    var accessToken: AccessToken = AccessToken.getCurrentAccessToken()
+//                    var isLoggedIn: Boolean = accessToken != null && !accessToken.isExpired
+//                }
+//
+//                override fun onCancel() {
+//                    // App code
+//                }
+//
+//                override fun onError(exception: FacebookException) {
+//                    // App code
+//                }
+//            })
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
+    class WebClient: WebViewClient() {
+
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            if (request != null) {
+                Log.d("InstagramTest", "url : " + request.url)
+                // TODO redirect 처리 후 앱 동작 처리
+            }
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
     }
 
 }
