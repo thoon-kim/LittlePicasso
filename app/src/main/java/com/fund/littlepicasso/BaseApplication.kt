4add7ed77fb3179b6dba54b7db897b5d07071bbd
dEpayse <bs5295@naver.com>
2021-03-05 14:47:02 +0900

    1. project 수준의 build.gradle – all projects, repositories에 코드 추가 (카카오SDK 레포지토리 설정)
    2. module 수준의 build.gradle - dependencies에 코드 추가 (카카오 로그인 설정)
    
    3. module 수준의 build.gradle - android에 코드 추가(viewBinding)
    
    4. KakaoLoginActivity.kt 에 코드 추가 (로그인 확인)
    
    5. activity_kakao_login.xml 화면 구성
    
    6. resource - drawble에 kakao_login_medium_narrow.png 파일 추가
    
    7. strings.xml에 kakao_app_key 추가
    
    8. BaseApplication.kt에 KakaoSDK 초기화 코드 추가

--- app/src/main/java/com/fund/littlepicasso/BaseApplication.kt
+++ app/src/main/java/com/fund/littlepicasso/BaseApplication.kt
@@ -1,11 +1,14 @@
 package com.fund.littlepicasso
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 
 class BaseApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        // Kakao SDK 초기화
+        KakaoSdk.init(this@BaseApplication, getString(R.string.kakao_app_key))
     }
 
 }
--- app/src/main/java/com/fund/littlepicasso/ui/login/KakaoLoginActivity.kt
+++ app/src/main/java/com/fund/littlepicasso/ui/login/KakaoLoginActivity.kt
@@ -1,14 +1,61 @@
 package com.fund.littlepicasso.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.fund.littlepicasso.R
+import com.fund.littlepicasso.databinding.ActivityKakaoLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
 
 class KakaoLoginActivity: AppCompatActivity() {
-
+    companion object{
+        const val KAKAO_TAG ="kakaoTag"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_kakao_login)
+        val binding = ActivityKakaoLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var keyHash = Utility.getKeyHash(this)
+        Log.d(KAKAO_TAG,keyHash)
+
+        //콜백 함수 : 로그인 결과에 따라 필요한 동작과 예외 처리
+        val loginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if(error!= null){
+                Log.e("LoginTest", "로그인 실패", error)
+            }
+            else if(token!=null){
+                Log.i("LoginTest", "로그인 성공 ${token.accessToken}")
+                //사용자 정보 가져오기
+                UserApiClient.instance.me { user, error ->
+                    if(error!=null){
+                        Log.e(KAKAO_TAG, "사용자 정보 요청 실패", error)
+                    }
+                    else if(user!=null){
+                        Log.i(KAKAO_TAG, "사용자 정보 요청 성공" +
+                                "\n회원번호: ${user.id}" +
+                                "\n이메일: ${user.kakaoAccount?.email}" +
+                                "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                "\n간편가입 시간: ${user.synchedAt}" +
+                                "\n서비스 연결 완료 시간: ${user.connectedAt}" +
+                                "\nproperties: ${user.properties}" +
+                                "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                    }
+                }
+            }
+        }
+
+        binding.signInButton.setOnClickListener {
+            //카카오톡이 설치되어 있는지 확인
+            if(UserApiClient.instance.isKakaoTalkLoginAvailable(this@KakaoLoginActivity)){
+                //카카오톡으로 로그인
+                UserApiClient.instance.loginWithKakaoTalk(this@KakaoLoginActivity, callback = loginCallback)
+            } else{
+                //기본 웹 브라우저를 통해 카카오계정으로 로그인
+                UserApiClient.instance.loginWithKakaoAccount(this@KakaoLoginActivity, callback = loginCallback)
+            }
+        }
+
     }
 
 }
